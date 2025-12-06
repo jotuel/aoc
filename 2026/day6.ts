0eed9100c0dbbc06c9b84a6a1bb5551213db741c
Joonas Tuomi <joonastapanituomi@gmail.com>
2025-12-06 21:30:26 +0200

    day 6

---
+++ 2026/day6.ts
@@ -0,0 +1,36 @@
+const bytes = await Deno.readFile("/home/jwnz/Git/aoc/2026/day6.txt");
+const text = new TextDecoder().decode(bytes);
+const lines = text.split("\n");
+
+const part1 = new Array<number>(lines[lines.length - 1].length).fill(0);
+let part2 = new Array<number>(lines[lines.length - 1].length).fill(0);
+let index = 0;
+
+for (let i = 0; lines[lines.length - 1][i]; i++) {
+  switch (lines[lines.length - 1][i]) {
+    case "+":
+      for (const line of lines) {
+        if (!line[i] || line[i] === "+") break;
+        part1[index] += parseInt(line.slice(i));
+      }
+      console.log(part1[index]);
+      index++;
+      break;
+    case "*":
+      part1[index] = 1;
+      for (const line of lines) {
+        if (!line[i] || line[i] === "*") break;
+        part1[index] *= parseInt(line.slice(i));
+      }
+      console.log(part1[index]);
+      index++;
+      break;
+    default:
+      break;
+  }
+}
+let res = 0;
+for (const nbr of part1) {
+  res += nbr;
+}
+console.log(res);
