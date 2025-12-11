0e3016203be05e32516af40bd29e2e92feb2943d
Joonas Tuomi <joonastapanituomi@gmail.com>
2025-12-11 02:11:58 +0200

    just needed to connect all the sets after first greedy pass

--- 2025/day8.ts
+++ 2025/day8.ts
@@ -29,8 +29,10 @@ function len(v: Vec3): number {
 
 let networks = new Map<number, Set<Vec3>>();
 let establishedConnectionDist = new Set<number>();
-for (let added = 0; added < arr.length; added++) {
+for (let added = 0; added < 1000; added++) {
   let shortest = 10000000;
+  let v: Vec3 = { x: 0, y: 0, z: 0 },
+    v1: Vec3 = { x: 0, y: 0, z: 0 };
   for (const i of arr) {
     for (const j of arr) {
       if (i === j) continue;
@@ -38,24 +40,37 @@ for (let added = 0; added < arr.length; added++) {
       const cmp = len(vec);
       if (shortest > cmp && !establishedConnectionDist.has(cmp)) {
         shortest = cmp;
+        v = i;
+        v1 = j;
       }
     }
   }
   establishedConnectionDist.add(shortest);
   let bool = false;
   for (let k = 0; k < networks.size; k++) {
-    if (networks.get(k)?.has(i) || networks.get(k)?.has(j)) {
-      networks.get(k)?.add(i);
-      networks.get(k)?.add(j);
+    if (networks.get(k)?.has(v) || networks.get(k)?.has(v1)) {
+      networks.get(k)?.add(v);
+      networks.get(k)?.add(v1);
       bool = true;
       break;
     }
   }
   if (!bool) {
     networks.set(networks.size, new Set<Vec3>());
-    networks.get(networks.size - 1)?.add(i);
-    networks.get(networks.size - 1)?.add(j);
+    networks.get(networks.size - 1)?.add(v);
+    networks.get(networks.size - 1)?.add(v1);
   }
 }
 
-console.log(networks);
+const a = networks.values().toArray();
+a.forEach((b) =>
+  b.forEach((v) => {
+    a.forEach((c) => {
+      if (c.has(v)) {
+        c.forEach(b.add, b);
+      }
+    });
+  }),
+);
+
+a.forEach((b) => console.log(b.size));
